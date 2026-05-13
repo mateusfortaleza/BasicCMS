be48a73dd4941d81986739f91c5aa0ed9117029f
Mateus Helmer <mateusvillanova@protonmail.com>
2026-05-13 11:26:04 -0300

    Corrected for attributes to htmlFor

--- src/edit-components/edit-page.jsx
+++ src/edit-components/edit-page.jsx
@@ -8,13 +8,13 @@ export default function editPage({title}) {
         <div className="edit-page">
             <h1>{title}</h1>
 
-            <label for="title-input">Title: </label>
+            <label htmlFor="title-input">Title: </label>
             <input type="text" name="title" id="title-input" />
 
-            <label for="image-input">Image: </label>
+            <label htmlFor="image-input">Image: </label>
             <input type="file" name="image-input" id="image-input" className="image-input"/>
 
-            <label for="color-input">Background Color: </label>
+            <label htmlFor="color-input">Background Color: </label>
             <input type="color" name="color-input" id="color-input" />
 
         </div>
