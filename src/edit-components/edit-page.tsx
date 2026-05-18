8d0ff5b0ce42af7b54ab4654ec0b7aed88a01c29
Mateus Helmer <mateusvillanova@protonmail.com>
2026-05-18 09:37:07 -0300

    Changed imports

--- src/edit-components/edit-page.tsx
+++ src/edit-components/edit-page.tsx
@@ -1,23 +1,26 @@
 "use client"
-import { insertion } from "../dal/HeroCardDAO";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Button as ShadButton } from "@/components/ui/button"
 import { FieldSet, Field, FieldLabel, FieldGroup, FieldLegend } from "@/components/ui/field"
 import {Input} from "@/components/ui/input"
 
-export default function editPage({title}) {
+export default function editPage({title}: {title: string}) {
     const [titleInput, setTitleInput] = useState("");
     const [imageInput, setImageInput] = useState("");
     const [colorInput, setColorInput] = useState("");
+    const [linkInput, setLinkInput] = useState("")
 
-    function onTitleChange(event) {
+    function onTitleChange(event: ChangeEvent<HTMLInputElement>) {
         setTitleInput(event.target.value);
     }
-    function onImageChange(event) {
+    function onImageChange(event: ChangeEvent<HTMLInputElement>) {
         setImageInput(event.target.value)
     }
-    function onColorChange(event) {
+    function onColorChange(event: ChangeEvent<HTMLInputElement>) {
         setColorInput(event.target.value)
+    }
+    function onLinkChange(event: ChangeEvent<HTMLInputElement>) {
+        setLinkInput(event.target.value)
     }
 
     return (
@@ -27,27 +30,23 @@ export default function editPage({title}) {
                 <FieldGroup>
                     <Field>
                         <FieldLabel htmlFor="title-input">Title:</FieldLabel>
-                        <Input name="title" id="title-input"  />
+                        <Input name="title" id="title-input" onChange={onTitleChange} />
                     </Field>
                     <Field>
                         <FieldLabel>Image:</FieldLabel>
-                        <Input type="file" name="image-input" id="image-input" className="image-input" />
+                        <Input type="file" name="image-input" id="image-input" className="image-input" onChange={onImageChange} />
                     </Field>
                     <Field>
                         <FieldLabel>Color:</FieldLabel>
-                        <input type="color" name="color-input" id="color-input" />
+                        <input type="color" name="color-input" id="color-input" onChange={onColorChange} />
+                    </Field>
+                    <Field>
+                        <FieldLabel>Link:</FieldLabel>
+                        <input type="text" name="link-input" id="link-input" onChange={onLinkChange} />
                     </Field>
                 </FieldGroup>
                 <ShadButton>Submit</ShadButton>
             </FieldSet>
-
-            {/* <label htmlFor="image-input">Image: </label>
-            <input value={imageInput} onChange={onImageChange} />
-
-            <label htmlFor="color-input">Background Color: </label>
-            <input value={colorInput} onChange={onColorChange} />
-
-            <label htmlFor="Button"></label> */}
         </>
     )
 }
