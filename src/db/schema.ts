9a47697b9cd051b26622eb882bcb6ec017cec5d2
Mateus Helmer <mateusvillanova@protonmail.com>
2026-06-02 09:20:58 -0300

    feat: Created hero_card_fields table and modified the data transfer object para não incluir hero cards apagados

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,4 @@
 import { pgTable, integer, varchar, boolean, text, uuid } from "drizzle-orm/pg-core"
-import { Ingrid_Darling } from "next/font/google";
 
 export const heroCard = pgTable("hero_card", {
 	id: integer().primaryKey().generatedAlwaysAsIdentity({ name: "hero_card_id_seq", startWith: 1, increment: 1, minValue: 1, maxValue: 2147483647, cache: 1 }),
@@ -8,6 +7,7 @@ export const heroCard = pgTable("hero_card", {
 	title: varchar("title", { length: 100 }).notNull(),
 	link: varchar("link", { length: 255 }).notNull(),
 	isDeleted: boolean("is_deleted").notNull().default(false),
+	id_lang: varchar({length: 2}).notNull().default("en").references(() => language.langCode)
 });
 
 export const menu = pgTable("menu", {
@@ -20,6 +20,14 @@ export const menu = pgTable("menu", {
 
 export const language = pgTable("language", {
 	id: uuid().primaryKey().defaultRandom(),
-	language: varchar({length: 100}).notNull(),
-	langCode: varchar("lang_code", {length: 2}).notNull(),
+	language: varchar({length: 100}).notNull().unique(),
+	langCode: varchar("lang_code", {length: 2}).notNull().unique(),
+})
+
+export const heroCardFields = pgTable("hero_card_fields", {
+	id: uuid().primaryKey().defaultRandom(),
+	backgroundImage: text("background_image").notNull(),
+	overlayColor: varchar("overlay_color", { length: 7 }).notNull(),
+	title: varchar("title", { length: 100 }).notNull(),
+	link: varchar("link", { length: 255 }).notNull(),
 })
