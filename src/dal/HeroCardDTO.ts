99bae47d3a6e4cf081358ac3201ad278226d5ba7
Mateus Helmer <mateusvillanova@protonmail.com>
2026-06-02 10:18:27 -0300

    removed optional chaingin (?.)

--- src/dal/HeroCardDTO.ts
+++ src/dal/HeroCardDTO.ts
@@ -3,17 +3,17 @@ import { heroCard } from "@/db/schema";
 import { and, eq } from "drizzle-orm";
 
 export async function getAllHeroCards() {
-  const resultOfQuery = await getDb()?.select({id: heroCard.id, image_path: heroCard.backgroundImage, title_text: heroCard.title, color: heroCard.overlayColor, link: heroCard.link}).from(heroCard).where(eq(heroCard.isDeleted, false));
+  const resultOfQuery = await getDb().select({id: heroCard.id, image_path: heroCard.backgroundImage, title_text: heroCard.title, color: heroCard.overlayColor, link: heroCard.link}).from(heroCard).where(eq(heroCard.isDeleted, false));
   return resultOfQuery;
 }
 
 export async function getHeroCardById(id: number) {
-  const result = await getDb()?.select({id: heroCard.id, image_path: heroCard.backgroundImage, title_text: heroCard.title, color: heroCard.overlayColor, link: heroCard.link}).from(heroCard).where(and(eq(heroCard.id, id), eq(heroCard.isDeleted, false))).limit(1);
+  const result = await getDb().select({id: heroCard.id, image_path: heroCard.backgroundImage, title_text: heroCard.title, color: heroCard.overlayColor, link: heroCard.link}).from(heroCard).where(and(eq(heroCard.id, id), eq(heroCard.isDeleted, false))).limit(1);
   return result[0];
 }
 
 export async function insertHeroCard(image_path: string, title_text: string, color: string, link: string) {
-  return await getDb()?.insert(heroCard).values({backgroundImage: image_path, overlayColor: color, title: title_text, link: link})
+  return await getDb().insert(heroCard).values({backgroundImage: image_path, overlayColor: color, title: title_text, link: link})
 }
 
 export async function updateHeroCard(id: number, image_path: string, title_text: string, color: string, link: string) {
@@ -21,5 +21,5 @@ export async function updateHeroCard(id: number, image_path: string, title_text:
 }
 
 export async function deleteHeroCard(id: number) {
-  return await getDb()?.update(heroCard).set({isDeleted: true}).where(eq(heroCard.id, id))
+  return await getDb().update(heroCard).set({isDeleted: true}).where(eq(heroCard.id, id))
 }
